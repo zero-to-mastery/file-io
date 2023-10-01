cc2a5f22e26397b0049110e21f1e6d27489aa74a
Daniel Badura <daniel.badura@outlook.com>
2023-10-01 12:40:22 +0200

    Challenge 3 submission

--- submissions/Daniel-Badura/main.py
+++ submissions/Daniel-Badura/main.py
@@ -1,10 +1,10 @@
 from modules.count_songs import count_songs
 from modules.count_songs_key import count_songs_key
-
+from modules.count_occurances import count_occurances
 csv_path = "spotify-2023.csv"
 
 if __name__ == "__main__":
-    
+
     # Challenge One
     song_count = count_songs(csv_path)
 
@@ -16,8 +16,18 @@ if __name__ == "__main__":
         print(f"Number of songs : {song_count}")
     
     # Challenge Two
-    key_column = "key" # You can modify the column
+    column_name = "key" # You can modify the column
     key_value = "E"    # and the value to be counted
 
-    songs_in_key = count_songs_key(csv_path, key_column, key_value)
+    songs_in_key = count_songs_key(csv_path, column_name, key_value)
     print(f"Number of songs in the key of E: {songs_in_key}")
+
+    # Challange Three
+
+    result = count_occurances(csv_path, column_name)
+
+    if result:
+        most_common_value, most_common_count = result
+        print(f"The most common value in the specified column is '{most_common_value}' with {most_common_count} occurrences.")
+    else:
+        print("An error occurred. Please check the CSV file and column name.")
