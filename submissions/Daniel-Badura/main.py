614cb6f7d09acf811ba03d01e4d741c4d7a80db1
Daniel Badura <daniel.badura@outlook.com>
2023-10-01 18:59:47 +0200

    removed csv file

--- submissions/Daniel-Badura/main.py
+++ submissions/Daniel-Badura/main.py
@@ -1,10 +1,10 @@
 from modules.count_songs import count_songs
 from modules.count_songs_key import count_songs_key
 from modules.count_occurances import count_occurances
+
 csv_path = "spotify-2023.csv"
 
 if __name__ == "__main__":
-
     # Challenge One
     song_count = count_songs(csv_path)
 
@@ -14,10 +14,10 @@ if __name__ == "__main__":
         print("Something went wrong.")
     else:
         print(f"Number of songs : {song_count}")
-    
+
     # Challenge Two
-    column_name = "key" # You can modify the column
-    key_value = "E"    # and the value to be counted
+    column_name = "key"  # You can modify the column
+    key_value = "E"  # and the value to be counted
 
     songs_in_key = count_songs_key(csv_path, column_name, key_value)
     print(f"Number of songs in the key of E: {songs_in_key}")
@@ -28,6 +28,8 @@ if __name__ == "__main__":
 
     if result:
         most_common_value, most_common_count = result
-        print(f"The most common value in the specified column is '{most_common_value}' with {most_common_count} occurrences.")
+        print(
+            f"The most common value in the specified column is '{most_common_value}' with {most_common_count} occurrences."
+        )
     else:
         print("An error occurred. Please check the CSV file and column name.")
