143a5dc1162a3287b033cdffc747938bbc41d9fe
Barry Hanna <barryahanna@gmail.com>
2023-10-02 09:10:40 +0100

    Refactored into functions

--- submissions/barryhanna/main.js
+++ submissions/barryhanna/main.js
@@ -9,24 +9,40 @@ const spotifyData = parse(readFileSync('./spotify-2023.csv'), {
 	skip_empty_lines: true,
 });
 
-console.log(`Total number of songs: ${spotifyData.length}`);
-const songsInKey = spotifyData.filter(
-	(song) => song.key === KEY
-).length;
-console.log(`Number of songs in key of ${KEY}: ${songsInKey}`);
+const getNumberOfSongs = (songData) => songData.length;
 
-const totals = spotifyData.reduce((acc, val) => {
-	const artistName = val[COUNT_COLUMN];
-	if (!acc[artistName]) {
-		acc[artistName] = 1;
-	} else {
-		acc[artistName] += 1;
-	}
-	return acc;
-}, {});
+const getNumberOfSongsInKey = (songData, key) =>
+	songData.filter((song) => song.key === key).length;
 
-const sorted = Object.entries(totals).sort((a, b) => b[1] - a[1]);
+const getMostPopularArtist = (songData) => {
+	const sortedArtistsWithSongTotal = Object.entries(
+		songData.reduce((acc, val) => {
+			const artistName = val[COUNT_COLUMN];
+			if (!acc[artistName]) {
+				acc[artistName] = 1;
+			} else {
+				acc[artistName] += 1;
+			}
+			return acc;
+		}, {})
+	).sort((a, b) => b[1] - a[1]);
+	return {
+		artist: sortedArtistsWithSongTotal[0][0],
+		songs: sortedArtistsWithSongTotal[0][1],
+	};
+};
 
 console.log(
-	`The most popular artist is ${sorted[0][0]} with ${sorted[0][1]} songs.`
+	`Total number of songs: ${getNumberOfSongs(spotifyData)}`
+);
+console.log(
+	`Number of songs in key of ${KEY}: ${getNumberOfSongsInKey(
+		spotifyData,
+		KEY
+	)}`
+);
+
+const artistWithMostSongs = getMostPopularArtist(spotifyData);
+console.log(
+	`The most popular artist is ${artistWithMostSongs.artist} with ${artistWithMostSongs.songs} songs.`
 );
