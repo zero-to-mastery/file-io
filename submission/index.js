494a5d6a2f18142a7b90a9c8316f68a8b28272d5
posh101 <paulorife@gmail.com>
2023-10-03 17:03:00 -0700

    modified index.js

--- submission/index.js
+++ submission/index.js
@@ -1,43 +1,57 @@
-const { parse } = require('csv-parse');
-const fs = require('fs');
-const path = require('path');
+const { parse } = require('csv-parse')
+const fs = require('fs')
+const path = require('path')
 
-let appleArtist = [];
-let getAllKeyOfESongs = [];
+const spotifyArtists = []
+const songsInKeyOfE = []
 
-//find popular artist
-let analyzeColumn = 'artist(s)_name';
-const values = [];
+// find most popular artist
+const columnToAnalyze = 'artist(s)_name'
+const columnValues = []
+const counts = {}
+let mostCommonValue = null
+let maxCount = 0
 
-const keyOfE = (artist) => {
-  return artist['key'] === 'E';
-};
-
-function findPopularArtist() {
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(path.join(__dirname, 'data', 'spotify-2023.csv'))
-      .pipe(parse({ columns: true }))
-
-      .on('data', async (data) => {
-        appleArtist.push(data);
-
-        if (keyOfE(data)) {
-          getAllKeyOfESongs.push(data);
-        }
-      })
-
-      .on('data', async (row) => {
-        const newValues = row[analyzeColumn];
-        if (newValues) {
-          values.push(newValues);
-        }
-      })
-
-      .on('err', (err) => {
-        console.log(err);
-        reject(err);
-      });
-  });
+// function to find the number of songs in the key of E
+function keyOfE(artist) {
+  return artist['key'] === 'E'
 }
 
-console.log(`there are ${appleArtist.length} in this list`);
+fs.createReadStream(path.join(__dirname, 'data', 'spotify-2023.csv'))
+  .pipe(parse({
+    columns: true
+  }))
+  .on('data', (data) => {
+    spotifyArtists.push(data)
+
+    if (keyOfE(data)) {
+      songsInKeyOfE.push(data)
+    }
+  })
+  .on('data', (row) => {
+    const columnValue = row[columnToAnalyze];
+
+    if (columnValue) {
+      columnValues.push(columnValue);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err)
+  })
+  .on('end', () => {
+
+  
+  columnValues.forEach((value) => {
+    !counts[value] ? counts[value] = 1 : counts[value]++
+
+    if (counts[value] > maxCount) {
+      mostCommonValue = value;
+      maxCount = counts[value];
+    }
+  });
+
+  // Log the results to the console
+  console.log(`There are ${spotifyArtists.length} songs in this list.`)
+  console.log(`There are ${songsInKeyOfE.length} songs in the key of E.`)
+  console.log(`The most popular artist is ${mostCommonValue} with ${maxCount} songs.`);
+  })
