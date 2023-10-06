31cc07bf1d07383f1bfd6585888b4f466209e183
Truong Quy Hai <haiquytruong@gmail.com>
2023-10-06 19:44:11 +0700

    Solve challenge 3

--- submissions/haitruong-tech/index.js
+++ submissions/haitruong-tech/index.js
@@ -1,6 +1,22 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 
+const col = process.argv[2]; // col to count the occurrences
+
+/**
+ * Cache holds occurences count
+ * @type {{ [col_value]: count }}
+ */
+const occurences = {
+  /**
+   * E.g.
+   * "Latto, Jung Kook": 23,
+   */
+};
+let mostOccurrences = {
+  value: "",
+  count: 0,
+};
 let noOfSongs = 0;
 let songsWithKeyE = 0;
 
@@ -9,8 +25,22 @@ fs.createReadStream("../../spotify-2023.csv")
   .on("data", (song) => {
     noOfSongs++;
     if (song.key === "E") songsWithKeyE++;
+    if (col == null || song[col] == null) return;
+
+    // As `artist(s)_name` column may holds multiple artists,
+    // we need to split this column into multiple values
+    const colValues = song[col].split(", ");
+    colValues.forEach((value) => {
+      occurences[value] ??= 0;
+      occurences[value]++;
+      if (occurences[value] > mostOccurrences.count) {
+        mostOccurrences.value = value;
+        mostOccurrences.count = occurences[value];
+      }
+    });
   })
   .on("end", () => {
     console.log("Total songs in the file:", noOfSongs);
     console.log("Total songs in the file with key 'E':", songsWithKeyE);
+    if (col != null) console.log("Most common value:", mostOccurrences.value);
   });
