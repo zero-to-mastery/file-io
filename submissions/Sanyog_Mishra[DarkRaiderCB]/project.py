149f84ef99c27865a34c2f436794288e9fccae7e
Sanyog Mishra_1094 <sanyog16355@gmail.com>
2023-10-07 22:47:35 +0530

    Solution to challenge-1

---
+++ submissions/Sanyog_Mishra[DarkRaiderCB]/project.py
@@ -0,0 +1,11 @@
+#HACKTOBERFEST-2023
+#Python Code for FILE I/O CHALLENGE 1
+
+import csv  #Using csv module
+with open("spotify-2023.csv","r") as song_file:
+    read_data = csv.reader(song_file)
+    next(read_data)
+    ctr=0   #initialising Counter
+    for line in read_data:  
+        ctr=ctr+1
+    print(f"The total number of songs in spotify-2023.csv are: {ctr}")
