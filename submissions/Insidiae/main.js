8747d3745c2cff31ef11d69eb5ddd2ca9e710780
Kobe Ruado <insidiae423@gmail.com>
2023-10-09 23:47:09 +0800

    Add interactive inputs for challenges 2 and 3

--- submissions/Insidiae/main.js
+++ submissions/Insidiae/main.js
@@ -36,19 +36,57 @@ async function challenge() {
   d3.select(".song-count").text(dataset.length);
 
   //* 2. Identify the number of songs in the key of E
+  const keySelect = d3.select("#challenge-2");
+  const keyGroups = d3.groupSort(
+    dataset,
+    (d) => d.key,
+    (d) => d.key
+  );
+
   function getKeyCount(dataset, key) {
     return dataset.filter((d) => d.key === key).length;
   }
 
+  keySelect
+    .selectAll("option")
+    .data(keyGroups.filter((key) => key !== "Unknown"))
+    .join("option")
+    .attr("value", (d) => d)
+    .attr("selected", (d) => (d === "E" ? true : undefined))
+    .text((d) => d);
+
   d3.select(".song-key-count").text(getKeyCount(dataset, "E"));
+  keySelect.on("change", (event) => {
+    d3.select(".song-key-count").text(getKeyCount(dataset, event.target.value));
+  });
 
   //* 3. Count the occurrences of values in a specified column
   //* (e.g., artist names) and determine the most common value.
-  function getTableRows(dataset, column = "artist(s)_name") {
+  const columnSelect = d3.select("#challenge-3-options");
+  const optionProps = {
+    artistsName: { label: "Artist", format: (d) => d[0] },
+    releasedYear: { label: "Year Released", format: (d) => d[0] },
+    releasedMonth: {
+      label: "Month Released",
+      format: (d) => {
+        const date = new Date();
+        date.setMonth(d[0] - 1);
+
+        return date.toLocaleString("en-US", {
+          month: "long",
+        });
+      },
+    },
+    bpm: { label: "BPM", format: (d) => d[0] },
+    key: { label: "Key", format: (d) => d[0] },
+    mode: { label: "Mode", format: (d) => d[0] },
+  };
+
+  function getTableRows(dataset, column) {
     const frequencyTable = {};
 
     for (let row of dataset) {
-      if (column === "artist(s)_name") {
+      if (column === "artistsName") {
         for (let artist of row.artistsName) {
           frequencyTable[artist] = (frequencyTable[artist] || 0) + 1;
         }
@@ -60,33 +98,57 @@ async function challenge() {
     return Object.entries(frequencyTable).sort((d1, d2) => d2[1] - d1[1]);
   }
 
-  const tableRows = getTableRows(dataset);
-  const table = d3.select("#challenge-3");
+  function makeTable(option = "artistsName") {
+    const tableRows = getTableRows(dataset, option);
+    const table = d3.select("#challenge-3-result");
+    table.html("");
 
-  const columns = [
-    { label: "Artist", type: "text", format: (d) => d[0] },
-    { label: "Frequency", type: "number", format: (d) => d[1] },
-  ];
+    const columns = [
+      {
+        label: optionProps[option].label,
+        type: "text",
+        format: optionProps[option].format,
+      },
+      {
+        label: "Frequency",
+        type: "number",
+        format: (d) => d[1],
+      },
+    ];
 
-  table
-    .append("thead")
-    .append("tr")
-    .selectAll("thead")
-    .data(columns)
-    .join("th")
-    .text((d) => d.label)
-    .attr("class", (d) => d.type);
-
-  const body = table.append("tbody");
-
-  tableRows.forEach((d) => {
-    body
+    table
+      .append("thead")
       .append("tr")
-      .selectAll("td")
+      .selectAll("thead")
       .data(columns)
-      .join("td")
-      .text((column) => column.format(d))
-      .classed("top-count", d[1] === tableRows[0][1]);
+      .join("th")
+      .text((d) => d.label)
+      .attr("class", (d) => d.type);
+
+    const body = table.append("tbody");
+
+    tableRows.forEach((d) => {
+      body
+        .append("tr")
+        .selectAll("td")
+        .data(columns)
+        .join("td")
+        .text((column) => column.format(d))
+        .classed("top-count", d[1] === tableRows[0][1]);
+    });
+  }
+
+  columnSelect
+    .selectAll("option")
+    .data(Object.keys(optionProps))
+    .join("option")
+    .attr("value", (d) => d)
+    .attr("selected", (d) => (d === "artistsName" ? true : undefined))
+    .text((d) => optionProps[d].label);
+
+  makeTable();
+  columnSelect.on("change", (event) => {
+    makeTable(event.target.value);
   });
 }
 
