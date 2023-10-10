65c6af1fc0c908022d5f167edd224198dc622e52
DELL <rohansalgare4@gmail.com>
2023-10-10 11:17:47 +0530

    Added my script in python which performs first two tasks

---
+++ submissions/RohanSalgare/index.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import csv
+
+
+def total_num_of_songs():
+    df = pd.read_csv('spotify-2023.csv', encoding='latin1')
+    return len(df)
+
+def number_of_songs_in_key(key):
+    with open('spotify-2023.csv', 'r') as file:
+        reader = csv.reader(file)
+        columns = next(reader)
+        #storing the index of element 'key' in index variable
+        index = columns.index('key')
+
+        num_of_songs = 0
+        for row in reader:
+            if row[index] == key:
+                num_of_songs += 1
+
+        file.close()
+    return num_of_songs
+
+
+print(f"Total Number of songs : {total_num_of_songs()}")
+print(f"Total Number of songs in Key: {number_of_songs_in_key('E')}")
+
