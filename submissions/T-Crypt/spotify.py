303449b3601556598b29c8c00f7d467e1e6d8dcb
Trevin <132922589+T-Crypt@users.noreply.github.com>
2023-10-11 10:16:53 -0600

    refactored -- simplified

--- submissions/T-Crypt/spotify.py
+++ submissions/T-Crypt/spotify.py
@@ -1,22 +1,14 @@
 import pandas as pd
 
+# Read the CSV file into a pandas DataFrame with specified encoding
+df = pd.read_csv('./spotify-2023.csv', encoding='ISO-8859-1')
+
 def count_songs_in_key_of_e(dataframe):
     key_of_e_songs = dataframe[dataframe['key'] == 'E']
     return len(key_of_e_songs)
 
-def main(csv_file_path):
-    df = pd.read_csv(csv_file_path, encoding='ISO-8859-1')
+# Print the total number of songs in the file
+print("Number of songs:", len(df))
 
-    total_songs = len(df)
-    print(f'Total number of songs: {total_songs}')
-  
-    songs_in_key_of_e = count_songs_in_key_of_e(df)
-    print(f'Number of songs in the key of E: {songs_in_key_of_e}')
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <path_to_csv_file>")
-    else:
-        csv_file_path = sys.argv[1]
-        main(csv_file_path)
+# Print the number of songs in the key of E with appropriate text
+print("Total Number in Key of E:", count_songs_in_key_of_e(df))
