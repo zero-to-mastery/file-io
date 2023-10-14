352b991d80aeb9d0195cb0bda15aa76b65850306
rivka-yuliia <juliacreadora@gmail.com>
2023-10-14 19:26:46 +0300

    class based

--- submissions/rivka-levit/main.py
+++ submissions/rivka-levit/main.py
@@ -1,16 +1,42 @@
 import pandas as pd
 
-# Read the data from csv file
-use_cols = ['track_name', 'artist(s)_name', 'key']
-df = pd.read_csv('spotify-2023.csv', usecols=use_cols)
 
-# Count the values
-songs_qty = df.count('rows')['track_name']
-e_qty = df['key'].value_counts()['E']
+class SpotifyPlayList:
+    """Manage operations with play list."""
 
-# Log the results to the console
-print(f'There are {songs_qty} songs in this list.')
-print(f'There are {e_qty} songs in the key of E.')
-for artist, qty in df['artist(s)_name'].value_counts(ascending=False).items():
-    print(f'The most popular artist is {artist} with {qty} songs.')
-    break
+    def __init__(self, filepath):
+        self.df = pd.read_csv(filepath)
+
+    def get_songs_quantity(self) -> int:
+        """Return quantity of songs in the play list."""
+
+        return len(self.df)
+
+    def get_value_count(self, column_name: str, value: str) -> int:
+        """Return number of occurrences of the value in the column."""
+
+        column_data = self.df.loc[:, column_name]
+
+        return column_data.value_counts()[value]
+
+    def get_most_frequent_value(self, column_name: str) -> tuple:
+        """Return the most frequent value in the column."""
+
+        column_data = self.df.loc[:, column_name]
+
+        all_counts = column_data.value_counts(ascending=False)
+        max_value = all_counts.index[0]
+        max_value_count = all_counts.iloc[0]
+
+        return max_value, max_value_count
+
+
+playlist = SpotifyPlayList('spotify-2023.csv')
+artist, qty = playlist.get_most_frequent_value('artist(s)_name')
+
+print(f'There are {playlist.get_songs_quantity()} songs in this list.')
+
+print(f'There are {playlist.get_value_count("key", "E")} songs '
+      f'in the key of E.')
+
+print(f'The most popular artist is {artist} with {qty} songs.')
