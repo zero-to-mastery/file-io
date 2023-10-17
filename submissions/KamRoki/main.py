2cfe8610e3305fafa8137794b57f6429739ed3a2
Kamil Stachurski <kamil.stachurski@icloud.com>
2023-10-17 10:05:19 +0200

    Create folder and file main.py

---
+++ submissions/KamRoki/main.py
@@ -0,0 +1,4 @@
+# Read CSV file
+import pandas as pd
+
+df = pd.read_csv('/Users/kamil/Machine-Learning/file-io/file-io/submissions/KamRoki/spotify-2023.csv', delimiter = ',', encoding="ISO-8859-1")
