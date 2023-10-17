f22db837ec289ec7561dd237e6b85d8b0ffd2eb1
Kamil Stachurski <kamil.stachurski@icloud.com>
2023-10-17 11:03:32 +0200

    Done 2 chellanges and create new branch

--- submissions/KamRoki/main.py
+++ submissions/KamRoki/main.py
@@ -1,8 +1,16 @@
 # Read CSV file
 import pandas as pd
 
+def count_key_e(data):
+    counts = data.key.value_counts()['E']
+    return counts
+
 df = pd.read_csv('/Users/kamil/Machine-Learning/file-io/file-io/submissions/KamRoki/spotify-2023.csv', delimiter = ',', encoding="ISO-8859-1")
 
 # Challange 1
 number_of_songs = len(df)
 print('Number of songs in Spotify file: ', number_of_songs)
+
+# Challange 2
+number_of_E = count_key_e(df)
+print('Number of songs with key E: ', number_of_E)
