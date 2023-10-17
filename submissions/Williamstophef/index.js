dd1b6aa5deae55a5f07ffdc1ea55479ce90c95fe
Christopher Williams <williamstophef@gmail.com>
2023-10-16 21:40:34 -0700

    Finished challenege

--- submissions/Williamstophef/index.js
+++ submissions/Williamstophef/index.js
@@ -2,7 +2,6 @@
 //install fs and csv-parser
 import fs from "fs";
 import csv from "csv-parser";
-import { clear } from "console";
 
 //array to hold the dataset as it is parsed through
 const results = [];
@@ -11,6 +10,7 @@ let songCount = 0;
 
 //imports the CSV into the read stream
 fs.createReadStream("spotify-2023.csv")
+  //pipe is required for csv to process input to an output
   .pipe(csv())
   //event listener for data. Then passes in the data to a function to push it into the results array
   //Also increase the increment of the songCount
@@ -22,5 +22,7 @@ fs.createReadStream("spotify-2023.csv")
   //Triggered when the end of the readable stream is reached
   .on("end", () => {
     // console.log(results);
-    console.log(`The number of songs in the Spotify 2023 list is ${songCount}`);
+    console.log(
+      `The number of songs in the Spotify 2023 playlist is ${songCount}`
+    );
   });
