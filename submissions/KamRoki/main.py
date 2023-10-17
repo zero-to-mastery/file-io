5023a908db398ab7523872a229791c89de0698b2
Kamil Stachurski <kamil.stachurski@icloud.com>
2023-10-17 10:34:56 +0200

    Done 1 challange

--- submissions/KamRoki/main.py
+++ submissions/KamRoki/main.py
@@ -2,3 +2,7 @@
 import pandas as pd
 
 df = pd.read_csv('/Users/kamil/Machine-Learning/file-io/file-io/submissions/KamRoki/spotify-2023.csv', delimiter = ',', encoding="ISO-8859-1")
+
+# Challange 1
+number_of_songs = len(df)
+print('Number of songs in Spotify file: ', number_of_songs)
