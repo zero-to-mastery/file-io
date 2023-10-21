cc3ab6cf023f66518d974a3842d653a855288f7f
manuelachamoso <manuelachamoso@gmail.com>
2023-10-21 12:54:59 +0200

    Adding my solution

--- submissions/Manuela-Chamoso/spotify_data_logger.js
+++ submissions/Manuela-Chamoso/spotify_data_logger.js
@@ -1,17 +1,14 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-// Specify the path to the CSV file
-const csvFilePath = 'C:\\Users\\Manug\\OneDrive\\Área de Trabalho\\file-io\\file-io\\spotify-2023.csv';
+const csvFilePath = 'spotify-2023.csv';
 
-// Function to read and process the CSV file
 function analyzeSpotifyData() {
   const songsInKeyE = [];
 
   fs.createReadStream(csvFilePath)
     .pipe(csv())
     .on('data', (row) => {
-      // Assuming the key column is named "key" in the CSV file
       if (row.key === 'E') {
         songsInKeyE.push(row);
       }
@@ -21,5 +18,4 @@ function analyzeSpotifyData() {
     });
 }
 
-// Run the data analysis
 analyzeSpotifyData();
