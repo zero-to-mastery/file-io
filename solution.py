56a3aaace2718f88ec5da778f4f8d123d8586cf0
SPANDAN PADHI <123059161+spandan0724@users.noreply.github.com>
2023-10-28 14:47:25 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,47 +1,28 @@
-# import module
 import pandas as pd
+from collections import Counter
 
-def count_songs_with_key(data):
-    counts = data['key'].value_counts()['E']
+def count_songs_with_key(data, key):
+    counts = data['key'].value_counts().get(key, 0)
     return counts
 
-def count_occurences_of_values(data):
-    dict = {}
-    artists = data['artist(s)_name']
+def count_occurrences_of_values(data):
+    artist_names = data['artist(s)_name'].str.split(', ')
+    all_artists = [artist for sublist in artist_names for artist in sublist]
+    count_artists = Counter(all_artists)
+    most_common_artist = count_artists.most_common(1)[0][0]
+    return most_common_artist
 
-    for artist in artists:
-        name = ""
-        for x in artist:
-            if x != ',':
-                name+=x
-            else:
-                name = name.strip()
-                if dict.get(name) == None:
-                    dict[name] = 1
-                else:
-                    dict[name] = dict[name] + 1
-                name=""
-        name = name.strip()
-        if dict.get(name) == None:
-            dict[name] = 1
-        else:
-            dict[name] = dict[name] + 1
-     
-    keyMax = max(zip(dict.values(), dict.keys()))[1]
-    return(keyMax)
+# Read the CSV file
+results = pd.read_csv('spotify-2023.csv')
 
-# read the csv file
-results = pd.read_csv('spotify-2023.csv')  
+# Solution 1: Number of songs
+num_of_songs = len(results)
+print("Number of songs:", num_of_songs)
 
+# Solution 2: Number of songs with key E
+num_of_songs_with_E = count_songs_with_key(results, 'E')
+print("Number of songs with key E:", num_of_songs_with_E)
 
-# solution 1
-numOfSongs = len(results)
-print("Number of songs: ", numOfSongs)
-
-# solution 2
-numOfSongsWithE = count_songs_with_key(results)
-print("Number of songs with key E: ", numOfSongsWithE)
-
-# solution 3
-mostOccurences = count_occurences_of_values(results)
-print("Most streamed artist: ", mostOccurences)
+# Solution 3: Most streamed artist
+most_occurrences = count_occurrences_of_values(results)
+print("Most streamed artist:", most_occurrences)
