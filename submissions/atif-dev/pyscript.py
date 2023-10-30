414315a3dd1888853204a7253233fd1186d1b632
atif-dev <ati1254@gmail.com>
2023-10-30 14:39:08 +0500

    added solution for 2nd challenge

--- submissions/atif-dev/pyscript.py
+++ submissions/atif-dev/pyscript.py
@@ -8,6 +8,7 @@ import time
 print(f'***************************HacktoberFest-2023*****************************')
 
 first_column_values = []
+key_column_values = []
 
 # Open CSV file for reading
 with open('spotify-2023.csv', 'r') as file:
@@ -17,6 +18,7 @@ with open('spotify-2023.csv', 'r') as file:
     # Iterate through the rows of .csv
     for row in reader:
         first_column_values.append(row[0])
+        key_column_values.append(row[15])
     
 #################Total Songs#######################
 count = 0;
@@ -25,4 +27,17 @@ for x in first_column_values:
         count = count + 1
 print(f'Total Songs: {count}')
 
+print(f'************************************************')
+#################Songs in  Key of E################
+ErowCount = 0;
+for keyValue in key_column_values:
+    if keyValue != "key":
+        if keyValue == "E":
+            ErowCount = ErowCount + 1
+print(f'Total Songs in key of E: {ErowCount}')
+
+print(f'************************************************')
+
 print(f'**********************************END*************************************')
+
+
