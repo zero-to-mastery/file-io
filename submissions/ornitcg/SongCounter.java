19bc91d3d8aade3325356c8b5e2a5f73e328775d
OCG <40301816+ornitcg@users.noreply.github.com>
2023-10-31 22:12:18 +0200

    improved spotify analysis  using JAVA

--- submissions/ornitcg/SongCounter.java
+++ submissions/ornitcg/SongCounter.java
@@ -1,26 +1,48 @@
 import java.io.*; 
 import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.HashMap;
 
 public class SongCounter {
-    public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(new File("./spotify-2023.csv"));  
-            sc.useDelimiter(","); 
-            // System.out.println("csv successfuly loaded");
-            int counter = 0;
-            while(sc.hasNext()){
-                counter++;
-                // System.out.print(sc.next());
-                sc.next();
+    public static int findIndex(String str, String[] searchArray){
+        for (int i=0; i< searchArray.length ; i++){
+            if(searchArray[i].equals(str)){
+                return i;
             }
-            System.out.println("csv contains " + counter + " songs");
-            sc.close();
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println("failed to load csv");
+        }
+        return -1;
+    } //findIndex
+    
+    public static void main(String[] args) {
+        String path = "./spotify-2023.csv";
+        try {
+            BufferedReader reader =new BufferedReader(new FileReader(path));
+         
+            String line = "";
+            line=reader.readLine();
+            String[] titles = line.trim().split(",");
+            int keyLocation= findIndex("key", titles);            
+            int songsCounter = 0;
+            int eKeyCounter = 0;
             
-        }       
-        
+            while((line=reader.readLine())!=null){
+                songsCounter++;
+                String[] songData = line.trim().split(",");
+                if (songData[keyLocation].equals("E")){
+                    eKeyCounter++;
+                }
+                
+            }
+            
+            System.out.println("Number of songs: "+ songsCounter);
+            System.out.println("Number of songs with E key: "+ eKeyCounter);
+            
+            
+            
+            
+        }
+        catch(Exception e){
+            System.out.println("csv failed to load");
+            
+        }
     }
 }
